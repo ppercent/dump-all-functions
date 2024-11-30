62c442efaaedfb58332c71b4aabc134e76a47798
ppercent <70774607+ppercent@users.noreply.github.com>
2024-11-30 20:30:03 +0100

    Added function signature

--- dumpFunctions.py
+++ dumpFunctions.py
@@ -14,22 +14,37 @@ def get_string_refs(decompilation):
 def get_function_base(addr):
     return idaapi.get_func(addr).start_ea if idaapi.get_func(addr) else addr
 
-# def get_xrefs_from(ea):
-#     xrefs = []
-#     function = idaapi.get_func(ea)
-#     if not function:
-#         return xrefs
+def get_function_signature(function_decomp):
+    # uses regex to find the function signature (return type, args)
+    # returns a dict {'args' : ['type', 'type'], 'return_type' : 'type'}
+    signature = {}
     
-#     # check all instruction of the function, and if its a call.
-#     for head in idautils.Heads(function.start_ea, function.end_ea):
-#         if idaapi.is_call_insn(head):
-#             for ref in idautils.XrefsFrom(head, 0):
-#                 if ref.type in [ida_xref.fl_CN, ida_xref.fl_CF]:
-#                     target_func = idaapi.get_func(ref.to)
-#                     if target_func:
-#                         xrefs.append(hex(target_func.start_ea))
-    
-#     return list(set(xrefs))
+    split_space_pattern = r'\S+'
+    split_args_pattern = r'\([\s\S]+?\)'
+    split_decomp = re.findall(split_space_pattern, function_decomp)
+    split_args = re.findall(split_args_pattern, function_decomp)
+
+    # get return type
+    return_type_arr = []    # TODO maybe put it back into a string instead of an array
+    for element in split_decomp:
+        if element.count('(') > 0:
+            break
+        if not element.count('fastcall') > 0:
+            return_type_arr.append(element)
+
+    # get args type
+    args_type_arr = []
+    for arg in split_args[0].split(','):
+        args = arg.split(' ')
+        args.pop()
+        print(args)
+        args_type_arr.append(' '.join([element for element in args if element]).replace('(', ''))
+
+    # update signature
+    signature['args'] = args_type_arr
+    signature['retval'] = ' '.join(return_type_arr)
+
+    return signature
 
 def get_xrefs_from(ea):
     xrefs = []
@@ -72,6 +87,7 @@ def dump_function(function_ea):
                 'decompilation' : function_decomp,
                 'strings' : [string for string in get_string_refs(function_decomp)],
                 'xrefs_to' : [hex(get_function_base(xref.frm)) for xref in list(idautils.XrefsTo(function_ea))],
+                'signature' : get_function_signature(function_decomp),
                 'xrefs_from' : get_xrefs_from(function_ea)
             }
         return function_dump
@@ -102,7 +118,7 @@ def dump_all():
     with open(f'{binary_name}_dump.json', 'w') as dump:
         json.dump(function_dump, dump, indent=2)
     
-    print(f'[+] Dumped {function_count - failed_decomp} / {function_count} functions')
+    print(f'[+] Dumped {function_count - failed_decomp} / {function_count} functions, have a wonderful day!')
 
 
 dump_all()
